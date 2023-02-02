8f588e7d491edb7dd4e2c4e0e488597dcbb94abe
Yago Andrade <yagoandradelima@outlook.com>
2023-02-02 17:35:29 -0300

    Update numpy_start_#3.py

--- numpy_start_#3.py
+++ numpy_start_#3.py
@@ -70,3 +70,60 @@ print()
 print('Divisão de arrays')
 print(a2/a3)
 print()
+
+
+
+#Para realizar uma multiplicação entre matrizes existe uma regra
+#A quantidade de linhas de uma matriz tem que possuir o mesmo número de colunas da segunda
+
+
+#Travando a matriz em uma seed
+np.random.seed(2)
+#Criando uma matriz, perceba que ao final coloquei o shape pra fazer ela ser bidimensional
+n1 = np.random.randint(1,16,(2,4))
+#travando matriz 2 em uma seed
+np.random.seed(3)
+#Criando uma matriz bidimensional. Perceber que linhas e colunas coincidem entre si
+n2 = np.random.randint(1, 16, (4,1))
+
+print('Matriz n1')
+print(n1)
+print()
+print('Matriz n2')
+print(n2)
+print()
+
+#multiplicando as matrizes
+print('Multiplicando matrizes')
+#Como coloquei n1...(n2) então, para esse caso, a COLUNA do shape declarado de N1,
+#precisa ser igual à LINHA do shape declarado em N2. Caso fosse n2...(n1), o 
+#inverso deveria ser verdade
+print(n1.dot(n2))
+print()
+
+#Comparando arrays
+print('Comparando elemento a elemento')
+r1 = np.array([1, 2, 3])
+r2 = np.array([3, 2, 3])
+print(r1 == r2)
+print()
+
+print('Comparando a igualdade de dois arrays completos')
+print(np.array_equal(r1,r2))
+print()
+
+print('Seno de r1')
+print(np.sin(r1))
+print()
+
+#Printando uma multiplicação dentro de um slice
+#Se a documentaçãod do numpy pedir "defined axis"
+#É só definir um slice na matriz daquilo que você quer ver
+print('Printando um produto entre determinados elementos do array')
+print(np.prod(n1[0,0:2]))
+print()
+
+#printando o desvio padrão
+print('Desvio padrão de um array')
+print(np.std(a1))
+print()
