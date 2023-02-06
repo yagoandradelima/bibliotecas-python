7997cee1bfed0ec02bd8ea8d65f3f9416ffa2e69
Yago Andrade <yagoandradelima@outlook.com>
2023-02-06 11:49:08 -0300

    Update pandas_start_#3.py

--- pandas_start_#3.py
+++ pandas_start_#3.py
@@ -7,5 +7,29 @@ print(array)
 print()
 df = pd.DataFrame(data=array, index=['Pedro', 'Ricardo', 'Luana'], columns=['Peso', 'Altura', 'Idade'])
 print(df)
+print()
 
+#Lembrar que os resultados do acesso retornam uma visulização de SERIES
+print('Printando acessando os valores de uma COLUNA')
+print(df['Altura'])
+print()
+
+print('Acessando uma linha')
+print(df.loc['Pedro'])
+print()
+
+print('Acessando um elemento específico do dataframe')
+print('A idade de Luana é', df['Idade']['Luana'], 'anos')
+print()
+
+print('Uma outra maneira de selecionar dados')
+print(df.loc['Luana']['Idade'])
+print()
+
+print('Selecionando duas colunas')
+print(df[['Idade', 'Peso']])
+print()
+
+print('Acessando os índices através dos números de indexação')
+print(df.iloc[2][1])
 print()
