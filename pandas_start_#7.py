23a1a4a1bc7a3764d135782a2e6fbb498c66ccd2
Yago Andrade <yagoandradelima@outlook.com>
2023-02-10 09:44:36 -0300

    Update pandas_start_#7.py

--- pandas_start_#7.py
+++ pandas_start_#7.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 
-dados = np.array([ 1,  2,  np.nan],
+dados = np.array([[ 1,  2,  np.nan],
                  [12,  np.nan,  np.nan],
-                 [ 3,  5,  2])
+                 [ 3,  5,  2]])
 
 df = pd.DataFrame(data=dados, columns='A B C'.strip())
+print(df)
