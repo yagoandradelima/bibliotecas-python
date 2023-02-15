b68feb902774e7065312690ca264f8c7d0a1d50a
Yago Andrade <yagoandradelima@outlook.com>
2023-02-14 22:19:40 -0300

    Update matplotlib_start_#2.py

--- matplotlib_start_#2.py
+++ matplotlib_start_#2.py
@@ -15,4 +15,21 @@ plt.plot(x, y1, label='Seno de x') #Plotando função 1 no gráfico 1. Colocando
 plt.plot(x, y2, label= 'Cosseno de x') #Plotando função 2 no gráfico 1. Colocando legenda
 plt.plot(x, y3, label='Seno de x + 3*seno de x') #Plotando função 3 no gráfico 1. Colocando legenda
 plt.legend(loc='upper right') #Solicitando legenda no gráfico. Mudando posicionamento (default = lower left)
+plt.xticks(np.arange(0,11,1)) #Plotando os números indicadores do eixo x
+plt.yticks(np.arange(-1, 4, 1)) #Plotando os números indicadores do eixo y
 plt.show() #Pedindo pra mostrar o gráfico
+
+#SUBPLOTS
+#Plotagens de gráficos comparativos a partir de dados maiores
+#Exemplo: as três linhas do gráfico anterior ter cada uma um gráfico para si
+
+plt.subplot(1, 3, 1) #Legenda: plt.subplot(Número de linhas de apresentação, número de colunas de apresentação, região que quero mostrar)
+plt.plot(x, y1) #Plotando dentro do subplot uma única linha
+plt.subplot(1, 3, 2) #Colocando o gráfico na posição 2 das 3 disponíveis
+plt.plot(x, y2)
+plt.subplot(1, 3, 3) #Colocando o gráfico na posição 3 das 3 disponíveis
+plt.plot(x, y3)
+plt.figure(figsize=(12,6)) #Passar uma tupla (1,2) no figsize!
+plt.show()
+
+
